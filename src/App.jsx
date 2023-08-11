fc347c35b8cbe417917800bd62ea4fa97a53ab07
pearlKinn <jinju000202@naver.com>
2023-08-11 19:55:57 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from './components/Button'
+import '@/styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Button status='Default' />
+        <Button status='Hover' />
+        <Button status='OnClick' />
+    </div>
+  );
+}
+
+export default App;
